0a4d625dfce1b133cf4cdaddc8faf8ea0ef29ab2
syuvivida <syu@phy.ncu.edu.tw>
2021-04-15 14:52:40 +0800

    obtain the signal power 95% CL limit assuming Gaussian distribution

---
+++ limits/CLs_Gauss.C
@@ -0,0 +1,14 @@
+#include <TMath.h>
+#include <TF1.h>
+
+// for 95% CL limits, TF1::GetX(0.05), for Poisson distribution
+Double_t CLs(Double_t* xx, Double_t* par){
+
+  Double_t mu_s=xx[0]; // mu_s, now set mu_b=0; sigma_b=1;
+  Double_t obs =par[0]; // in terms of sigma_b, can range from -inf to inf
+
+  Double_t CLspb = (1+TMath::Erf((obs-mu_s)/sqrt(2)))*0.5;
+  Double_t CLb = (1+TMath::Erf((obs)/sqrt(2)))*0.5;
+  Double_t CLs = CLb>1e-16? CLspb/CLb:-1;
+  return CLs;
+}
