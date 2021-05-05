70e916838f8b8be350e5df30ffa96ccbcd669f21
syuvivida <syu@phy.ncu.edu.tw>
2021-05-05 17:13:59 +0800

    macro to compute limits using CLS method

---
+++ limits/CLs_limit.C
@@ -0,0 +1,111 @@
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TF1.h>
+#include <TGraph.h>
+#include <iostream>
+#include <TTree.h>
+#include <TChain.h>
+#include <TBranch.h>
+#include <TLeaf.h>
+#include "compute.C"
+
+using namespace std;
+
+const double kB_constant=1.38e-23; //1.380649e-23; // in J*K-1
+const double noiseT =3 ; // in K
+const double binSize = 1000; // in Hz
+const double nspec = 52443000;
+const double mean_noise_exp = kB_constant*noiseT*binSize;
+const double sigmaN_exp= mean_noise_exp/sqrt(nspec);
+
+// for 95% CL limits, TF1::GetX(0.05), for Gaussian distribution
+// TF1* f1 = new TF1("f1", CLs,-10,10,1);
+// f1->SetParameter(0,xxx) where xxx is (observed power - noise_mean)/sigma
+// f1->GetX(0.05) gives the 95% CL upper limit on signal power in terms of sigma
+Double_t CLs_Gaus(Double_t* xx, Double_t* par){
+
+  Double_t mu_s=xx[0]; // mu_s, now set mu_b=0; sigma_b=1;
+  Double_t obs =par[0]; // in terms of sigma_b, can range from -inf to inf
+
+  Double_t CLspb = (1+TMath::Erf((obs-mu_s)/sqrt(2)))*0.5;
+  Double_t CLb = (1+TMath::Erf((obs)/sqrt(2)))*0.5;
+  Double_t CLs = CLb>1e-16? CLspb/CLb:-1;
+  return CLs;
+}
+
+void CLs_limit(std::string inputFile, std::string treeName="data")
+{
+
+
+
+  TChain* thisTree = new TChain(treeName.data());
+
+  thisTree->Add(inputFile.data());
+
+
+  Long64_t nentries = (Long64_t)thisTree->GetEntries();
+  TH1F* hlimit = new TH1F("hlimit","95% CLs limit on signal power",200,-10,10);
+  TH1F* hobs_sigma = new TH1F("hobs_sigma","Observed power difference from noise in terms of #sigma_{N}",100,-5,5);
+  TF1* f1 = new TF1("f1",CLs_Gaus,-10,10,1);
+  f1->SetNpx(5000);
+  const unsigned int nBins = nentries;
+  double x[nBins], y[nBins];
+  double y_gaAA[nBins];
+  double y_ratio[nBins];
+  cout << "Expected mean_noise = " <<  mean_noise_exp << endl;
+  cout << "Expected #sigma_N = " << sigmaN_exp << endl;
+
+  double mean_obs = 0;
+  
+  // loop over event entries
+  for (Long64_t jentry=0; jentry<nentries;jentry++) {
+    // this line is very important!!
+    thisTree->GetEntry(jentry);
+    double frequency=-9999, power_spectrum=-9999;
+    frequency      = thisTree->GetBranch("frequency")->GetLeaf("frequency")->GetValue();
+    power_spectrum = thisTree->GetBranch("power_spectrum")->GetLeaf("power_spectrum")->GetValue();
+
+    mean_obs += power_spectrum;
+    
+    double obs_diffFromNoise_sigma = (power_spectrum - mean_noise_exp)/sigmaN_exp;
+    f1->SetParameter(0, obs_diffFromNoise_sigma);
+    double limit =f1->GetX(0.05);
+    x[jentry] = frequency/1e9;
+    y[jentry] = limit; // in units of sigmaN
+    hlimit->Fill(limit);
+    hobs_sigma->Fill(obs_diffFromNoise_sigma);
+
+    double limit_on_gaAA = computeLimit(limit,frequency);
+    y_gaAA[jentry] = limit_on_gaAA;
+
+    double limit_KSVZ = computegAgg(frequency);
+    double ratio = limit_on_gaAA/limit_KSVZ;
+    y_ratio[jentry] = ratio;
+
+  } // end of loop over entries
+
+  mean_obs /= nentries;
+  cout << "Measured mean_noise = " <<  mean_obs << endl;
+
+  hlimit->Draw();
+  hobs_sigma->Draw();
+  
+  TGraph* g_limit = new TGraph(nBins, x,y);
+  g_limit->GetXaxis()->SetTitle("Frequency [GHz]");
+  g_limit->GetYaxis()->SetTitle("95% CLS limit on signal power in terms of #sigma_{N}");
+  g_limit->Draw("AC*");
+
+
+  TGraph* gaAA_limit = new TGraph(nBins, x,y_gaAA);
+  gaAA_limit->GetXaxis()->SetTitle("Frequency [GHz]");
+  gaAA_limit->GetYaxis()->SetTitle("95% CLS limit on g_{a#gamma#gamma} [GeV^{-1}]");
+  gaAA_limit->Draw("AC*");
+
+  TGraph* g_ratio = new TGraph(nBins, x,y_ratio);
+  g_ratio->GetXaxis()->SetTitle("Frequency [GHz]");
+  g_ratio->GetYaxis()->SetTitle("95% CLS limit on g_{a#gamma#gamma}/ g_{a#gamma#gamma}^{KSVZ}");
+  g_ratio->Draw("AC*");
+
+  
+}
--- limits/compute.C
+++ limits/compute.C
@@ -17,9 +17,10 @@ const double inverse_mu0pi = 0.5e7;
 const double jouelToeV=1/1.6e-19;
 
 //compute gAgammagamma
-void computegAgg(const double ma,                 // in eV
+double computegAgg(const double f,                 // in Hz
 		 const double gGamma=-0.97)       // KSVZ
 {
+  double ma = h*f*jouelToeV;
   double gAgammagamma = fabs(gGamma)*alpha/pi/lambda/lambda*ma; // in eV-1
 
   gAgammagamma *= 1e9;
@@ -34,11 +35,14 @@ void computegAgg(const double ma,                 // in eV
   cout << "DFSZ gAgammagamma for ma= " << ma*1e6 << " micro eV is "
        << (0.203*8./3.-0.39)*ma*1e-9 << " GeV-1" << endl;
 
+  return  gAgammagamma;
+
 }
 
 
 // limit on gAgammagamma
-double computePs(const double f0=5e9,             // in Hz
+double computePs(const double f=5e9,               // scan frequency in Hz
+		 const double f0=5e9,              // resonance frequency
 		 const double beta=1,
 		 const double B=9,                // in Tesla
 		 const double V=1e-3,             // in m^3
@@ -52,8 +56,8 @@ double computePs(const double f0=5e9,             // in Hz
   // here, compute fluctuation of noise power to be one photon energy
 
   double Psig =pow(gGamma*alpha/pi/lambda/lambda,2)
-    *pow(hbarc,3)*rhoDM*beta/(1+beta)*f0*inverse_mu0pi
-    *B*B*V*Cmnl*QL;
+    *pow(hbarc,3)*rhoDM*beta/(1+beta)*f*inverse_mu0pi
+    *B*B*V*Cmnl*QL*(1/(1+pow(2*QL*(f/f0-1),2)));
 
   cout << "signal power = " << Psig << endl;
   return Psig;
@@ -61,17 +65,18 @@ double computePs(const double f0=5e9,             // in Hz
 }
 
 
-void computeLimit(const double significance=1.645,
- 		  const double f0=5e9,             // in Hz
- 		  const double Tsys=-1,          // in K
- 		  const double beta=1,
- 		  const double B=9,                // in Tesla
- 		  const double V=1e-3,             // in m^3
- 		  const double Cmnl=0.5,            
- 		  const double QL=50000,
- 		  const double bandwidth=5e4,      // in Hz
+double computeLimit(const double significance=1.645,
+		  const double f=5e9,              // scan frequency
+ 		  const double f0=4.732e9,        // resonance frequency in Hz
+ 		  const double Tsys=3,          // in K
+ 		  const double beta=1/3.04,
+ 		  const double B=8,                // in Tesla
+ 		  const double V=234111e-9,             // in m^3
+ 		  const double Cmnl=0.65,            
+ 		  const double QL=9000,
+ 		  const double bandwidth=5e3,      // in Hz
  		  const double intT=3600,
-		  const int nspec=0)          // in seconds
+		  const int nspec=52443000)          // in seconds
  {
 
    // here, compute fluctuation of noise power to be one photon energy
@@ -84,26 +89,29 @@ void computeLimit(const double significance=1.645,
 
    double upperLimit_power = sigmaN*significance;
 
-   double ma = h*f0*jouelToeV;
+   double ma = h*f*jouelToeV;
    cout << "mass of axion is " << ma << " eV" << endl;
    
    
    double factor_signal_power = 1/pow(ma,2)
-     *pow(hbarc,3)*rhoDM*beta/(1+beta)*f0*inverse_mu0pi
-     *B*B*V*Cmnl*QL;
+     *pow(hbarc,3)*rhoDM*beta/(1+beta)*f*inverse_mu0pi
+     *B*B*V*Cmnl*QL*(1/(1+pow(2*QL*(f/f0-1),2)));
 
    double gAgammagamma = sqrt(upperLimit_power/factor_signal_power);
    // in eV-1
 
-   cout << "Upper limit on gAgammagamma = " << gAgammagamma*1e9 <<
-     " GeV-1" << endl;
+   gAgammagamma *=1e9; 
+   cout << "Upper limit on gAgammagamma = " <<  gAgammagamma << " GeV-1" << endl;
+
+   return gAgammagamma;
  }
 
 
 
 void computeSNR(
 		const double gGamma=-0.97,        // KSVZ
-		const double f0=5e9,             // in Hz
+		const double f=5e9,               // scan frequency
+		const double f0=5e9,             // resonance frequency in Hz
 		const double Tsys=-1,          // in K
 		const double beta=1,
 		const double B=9,                // in Tesla
@@ -123,7 +131,8 @@ void computeSNR(
 
    cout << "sigmaN = " << sigmaN << endl;
 
-   double signal_power = computePs(f0,
+   double signal_power = computePs(f,
+				   f0,
 				   beta,
 				   B,
 				   V,
